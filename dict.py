08eeee55adf002ba72d51dd28e3e45f3b871d266
chewkameka <42347557+chewkameka@users.noreply.github.com>
2019-05-21 11:21:46 +0530

    Python_Dictionary
    I have created and interactive dictionary using python. I used a .json file which has a dictionary consisting of huge collection of words. 
    The program takes input from the user and checks for the word in the json file. If it finds a match it returns it, otherwise it suggests words that closely match the input word. If none of them match, it prints that the word doesn't exist.

---
+++ dict.py
@@ -0,0 +1,33 @@
+# Created by Abhijith P Kumar
+
+import json
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+def meaning(w):
+    w = w.lower()
+    if w in data:
+        return  data[w]
+    elif len(get_close_matches(w,data.keys()))>0:
+        for i in range(0,3):
+            a = input("Did you mean %s instead? Y / N: " % get_close_matches(w,data.keys())[i])
+            a = a.upper()
+            if a == 'Y':
+                b = get_close_matches(w,data.keys())[i]
+                return meaning(b)
+            elif a == 'N' and i >= 3:
+                return "The word doesn't exist"
+            elif a != 'Y' and a != 'N':
+                return "Can't understand"
+    else:
+        return "The word doesn't exist"
+word = input("Enter a word: ")
+output = meaning(word)
+print("\n")
+if type(output) == list:
+    for i in output:
+        print(i)
+else:
+    if output is None:
+        print("Sorry, the word doesn't exist...")
+    else:
+        print(output)
